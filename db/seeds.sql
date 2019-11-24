762b5443a2801266ad17c8d83e510b1acd441db4
melodyulloa <50853884+melodyulloa@users.noreply.github.com>
2019-11-24 18:39:58 -0500

    completed hw

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Portobello Mushroom Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cripsy Chicken Burger', false);
+
